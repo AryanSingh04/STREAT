0c88a09b6608976af188d6591b4895090d1aa41a
Aryan Singh <aryanrrsingh.04@gmail.com>
2024-07-14 21:06:55 +0530

    First upload using git commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let ham=document.getElementById("menu")
+let items=document.getElementById("menu-items")
+let cross =document.getElementById("cross")
+const k=[ham,cross]
+console.log(items)
+k.forEach((e)=>{
+    e.addEventListener("click",()=>{
+        items.classList.toggle("active")
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.footer-section h3');
+
+    sections.forEach(section => {
+        section.addEventListener('click', function() {
+            const content = this.nextElementSibling;
+            content.style.display = content.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+});
