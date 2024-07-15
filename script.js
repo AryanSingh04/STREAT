81d80fde35de261994035cadc77d5af128e2c7de
Aryan Singh <aryanrrsingh.04@gmail.com>
2024-07-15 22:36:41 +0530

    added Footer

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ let items=document.getElementById("menu-items")
 let cross =document.getElementById("cross")
 const k=[ham,cross]
 console.log(items)
+function callAnim(){
+    gsap.from(".menanim *",{
+        y:100+"%",
+        stagger:0.2,
+    })
+}
 k.forEach((e)=>{
     e.addEventListener("click",()=>{
         items.classList.toggle("active")
+        if(items.classList.contains("active")){
+        document.body.style.height="100vh"
+        document.body.style.overflow="hidden"
+        callAnim()
+        }
+        else{
+        document.body.style.height="fit-content"
+        document.body.style.overflow="visible"
+        }
     })
 })
 
@@ -24,3 +39,4 @@ gsap.from(".anim *",{
     y:100+"%",
     stagger:0.5,
 })
+
